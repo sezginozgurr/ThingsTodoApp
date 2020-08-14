cace3a4f00cc751cdf1ec1e08e47bcafab3c7a2f
ozgurcan SEZGIN <sezginzgurr@gmail.com>
2020-08-14 21:43:20 +0300

    second commit

---
+++ app/src/main/java/com/example/thingstodoapp/Loginpage.kt
@@ -0,0 +1,11 @@
+package com.example.thingstodoapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Loginpage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loginpage)
+    }
+}
