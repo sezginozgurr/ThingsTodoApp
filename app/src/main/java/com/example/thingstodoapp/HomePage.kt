02b81094110a0a43d00d1fa9fda2b685fbfa346c
Burakcan Sezgin <burakcan.sezgin@gmail.com>
2020-08-18 15:29:23 +0300

    modify homePage - Add clickFun - add LoadFragment fun

--- app/src/main/java/com/example/thingstodoapp/HomePage.kt
+++ app/src/main/java/com/example/thingstodoapp/HomePage.kt
@@ -2,6 +2,7 @@ package com.example.thingstodoapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.example.thingstodoapp.fragment.DayFragment
 import com.example.thingstodoapp.fragment.MountFragment
@@ -17,33 +18,49 @@ class HomePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
-        val bottomNavigationBar: BottomNavigationView = findViewById(R.id.btn_nav)
-        bottomNavigationBar.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.home -> {
-                    dayFragment = DayFragment()
-                    supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.frame_layout, dayFragment)
-                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).commit()
-                }
-                R.id.favorite -> {
-                    weekFragment = WeekFragment()
-                    supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.frame_layout, weekFragment)
-                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).commit()
-                }
-                R.id.profile -> {
-                    mountFragment = MountFragment()
-                    supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.frame_layout, mountFragment)
-                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).commit()
-                }
-            }
-            true
-        }
+        loadFragment(DayFragment())
+        clickFunction()
+
     }
 
+
+    fun clickFunction() {
+        val bottomNavigationBar: BottomNavigationView = findViewById(R.id.btn_nav)
+        bottomNavigationBar.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            val fragment: Fragment
+            when (item.itemId) {
+                R.id.home -> {
+                    fragment = DayFragment()
+                    loadFragment(fragment)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.favorite -> {
+                    fragment = WeekFragment()
+                    loadFragment(fragment)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.profile -> {
+                    fragment = MountFragment()
+                    loadFragment(fragment)
+                    return@OnNavigationItemSelectedListener true
+                }
+//                R.id.profile -> {
+//                    fragment = ProfileFragment()
+//                    loadFragment(fragment)
+//                    return@OnNavigationItemSelectedListener true
+//                }
+            }
+            false
+        })
+    }
+
+
+    private fun loadFragment(fragment: Fragment) {
+        // load fragment
+        val transaction =
+            supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frame_layout, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
 }
