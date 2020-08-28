e429237dc8fd0635437aedf3dd478306ec1aff3c
ozgurcan SEZGIN <sezginzgurr@gmail.com>
2020-08-28 09:09:37 +0300

    a

--- app/src/main/java/com/example/thingstodoapp/fragment/DayFragment.kt
+++ app/src/main/java/com/example/thingstodoapp/fragment/DayFragment.kt
@@ -4,8 +4,8 @@ package com.example.thingstodoapp.fragment
 
 import android.app.AlertDialog
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.widget.AdapterView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.room.Room
@@ -24,6 +24,7 @@ class DayFragment : Fragment(R.layout.fragment_home_page) {
 
     private var detailFragment: DetailFragment? = null
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<*>
+    private var clickCard: Boolean = false
 
     private val bottomSheetCallback = object :
         BottomSheetBehavior.BottomSheetCallback() {
@@ -57,11 +58,11 @@ class DayFragment : Fragment(R.layout.fragment_home_page) {
         todoList = db.todoDao().getAllNotes()
         recycler_todo.adapter = TodoListAdapter(todoList) {
             alertDialog(it)
-
-            //navController?.navigate(R.id.action_homePageFragment_to_detailFragment)
         }
         (recycler_todo.adapter as TodoListAdapter).notifyDataSetChanged() // degisiklikden sonra ki hali
-        spinnersound.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+
+
+/*        spinnersound.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(p0: AdapterView<*>?) {
                 TODO("Not yet implemented")
             }
@@ -74,7 +75,7 @@ class DayFragment : Fragment(R.layout.fragment_home_page) {
             ) {
             }
 
-        }
+        }*/
         val bottomSheetBehavior = BottomSheetBehavior.from(layoutBottomSheet)
         fab_btn.setOnClickListener {
             if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_COLLAPSED) {
@@ -106,11 +107,52 @@ class DayFragment : Fragment(R.layout.fragment_home_page) {
 
     }
 
+/*    fun clickFunction() {
+        card1.setOnClickListener {
+            if (!clickCard) {
+                var lastPos = -1
+                if (lastPos == -1) {
+                    lastPos = position
+                    item.setChecked(true)
+                } else {
+                    val toModel = ToModel()
+                    item.setChecked(false)
+                    (recycler_todo.adapter as TodoListAdapter).updateItem(lastPos, toModel)
+                    lastPos = position
+
+                    item.setChecked(true)
+                    (recycler_todo.adapter as TodoListAdapter).updateItem(position, toModel)
+
+                }
+
+                clickCard = true
+                card1.setCardBackgroundColor(
+                    ContextCompat.getColor(
+                        card1.context,
+                        R.color.color_gray
+                    )
+                )
+            } else {
+                clickCard = false
+                card1.setCardBackgroundColor(
+                    ContextCompat.getColor(
+                        card1.context,
+                        R.color.color_white
+                    )
+                )
+            }
+
+        }
+    }*/
+
     fun alertDialog(toModel: ToModel): AlertDialog.Builder {
         val dialog = AlertDialog.Builder(view?.context)
         val dialogView = layoutInflater.inflate(R.layout.fragment_detail, null)
         val detailDay = dialogView.findViewById(R.id.detailDay) as TextView
-        detailDay.text = toModel.startDate
+        val step = getDateStep(toModel)
+        detailDay.text = step[0]
+        val detailClock = dialogView.findViewById(R.id.txt_clock) as TextView
+        detailClock.text = step[1]
         val detailTitle = dialogView.findViewById(R.id.detailTitle) as TextView
         detailTitle.text = toModel.todo
         val detailDescription = dialogView.findViewById(R.id.detailDescription) as TextView
@@ -122,6 +164,22 @@ class DayFragment : Fragment(R.layout.fragment_home_page) {
         }
         dialog.show()
         return dialog
+    }
+
+    fun getDateStep(todoModel: ToModel): List<String> {
+        val getDate = todoModel.startDate
+        val splitGetDate = " "
+
+        val resultDate = getDate.split(splitGetDate).toTypedArray()
+        Log.e("tag", resultDate[0])
+        val dateSplit = resultDate[0]
+
+        val lastSplit = "."
+        val resultDay = dateSplit.split(lastSplit).toTypedArray()
+        Log.e("tag", "${resultDay[0]}   }")
+        resultDate[0] = resultDay[0]
+
+        return resultDate.toList()
     }
 
 
